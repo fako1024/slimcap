bc2a94ab442c71c7d3f7ba8df8c896944a9ab231
fako1024 <>
2026-04-25 14:36:03 +0200

    Fix logging SDK errors

--- examples/pcap/main.go
+++ examples/pcap/main.go
@@ -7,9 +7,7 @@ package main
 import (
 	"errors"
 	"flag"
-	"fmt"
 	"io"
-	"os"
 
 	"github.com/els0r/telemetry/logging"
 	"github.com/fako1024/slimcap/capture/pcap"
@@ -23,44 +21,38 @@ func main() {
 		raw      bool
 	)
 
-	logger, logErr := logging.New(logging.LevelInfo, logging.EncodingPlain)
-	if logErr != nil {
-		fmt.Fprintf(os.Stderr, "failed to instantiate CLI logger: %v\n", logErr)
-		os.Exit(1)
-	}
-
 	flag.StringVar(&fileName, "f", "", "pcap file to read from")
 	flag.IntVar(&maxPkts, "n", 10, "maximum number of packets to process")
 	flag.BoolVar(&raw, "r", false, "output raw packet information")
 	flag.Parse()
 	if fileName == "" {
-		logger.Fatalf("no input file specified (-f)")
+		logging.Logger().Fatalf("no input file specified (-f)")
 	}
 
 	listener, err := pcap.NewSourceFromFile(fileName)
 	if err != nil {
-		logger.Fatalf("failed to start pcap reader for `%s`: %s", fileName, err)
+		logging.Logger().Fatalf("failed to start pcap reader for `%s`: %s", fileName, err)
 	}
 
 	defer func() {
 		if err := listener.Close(); err != nil {
-			logger.Fatalf("failed to close listener on `%s`: %s", fileName, err)
+			logging.Logger().Fatalf("failed to close listener on `%s`: %s", fileName, err)
 		}
 	}()
 
-	logger.Infof("Reading up to %d packets from `%s` (link type: %d)...", maxPkts, fileName, listener.Link().Type)
+	logging.Logger().Infof("Reading up to %d packets from `%s` (link type: %d)...", maxPkts, fileName, listener.Link().Type)
 	for i := 0; i < maxPkts; i++ {
 		p, err := listener.NextPacket(nil)
 		if err != nil {
 			if errors.Is(err, io.EOF) {
 				return
 			}
-			logger.Fatalf("error during packet reading (copy operation) on `%s`: %s", fileName, err)
+			logging.Logger().Fatalf("error during packet reading (copy operation) on `%s`: %s", fileName, err)
 		}
 		if raw {
-			logger.Infof("Read packet with Payload (total len %d): %v", p.TotalLen(), p.Payload())
+			logging.Logger().Infof("Read packet with Payload (total len %d): %v", p.TotalLen(), p.Payload())
 		} else {
-			logger.Infof("Read packet (total len %d): %v", p.TotalLen(), p.IPLayer().String())
+			logging.Logger().Infof("Read packet (total len %d): %v", p.TotalLen(), p.IPLayer().String())
 		}
 	}
 }
--- examples/trace/trace.go
+++ examples/trace/trace.go
@@ -6,10 +6,12 @@ import (
 	"os"
 	"os/signal"
 	"runtime/pprof"
+	"slices"
 	"sort"
 	"strings"
 	"sync"
 
+	"github.com/els0r/telemetry/logging"
 	"github.com/fako1024/gotools/link"
 	"github.com/fako1024/slimcap/capture"
 	"github.com/fako1024/slimcap/capture/afpacket/afpacket"
@@ -44,8 +46,8 @@ func (c *Capture) SkipIfaces(ifaces []string) *Capture {
 }
 
 // MaxIfaceErrors sets the maximum number of errors allowed to occur before capture is terminated
-func (c *Capture) MaxIfaceErrors(max int) *Capture {
-	c.maxIfaceErrors = max
+func (c *Capture) MaxIfaceErrors(maxErrs int) *Capture {
+	c.maxIfaceErrors = maxErrs
 	return c
 }
 
@@ -111,7 +113,7 @@ func (c *Capture) Run() (err error) {
 		return err
 	}
 	for _, iface := range links {
-		logger.Infof("Found interface `%s` (idx %d), link type %d", iface.Name, iface.Index, iface.Type)
+		logging.Logger().Infof("Found interface `%s` (idx %d), link type %d", iface.Name, iface.Index, iface.Type)
 	}
 
 	// construct list of skipped interfaces
@@ -156,13 +158,13 @@ func (c *Capture) Run() (err error) {
 		return skipped[i] < skipped[j]
 	})
 	if len(skipped) > 0 {
-		logger.Warnf("skipping capture on interfaces [%s]", strings.Join(skipped, ","))
+		logging.Logger().Warnf("skipping capture on interfaces [%s]", strings.Join(skipped, ","))
 	}
 
 	sort.Slice(capturing, func(i, j int) bool {
 		return capturing[i] < capturing[j]
 	})
-	logger.Infof("attempting capture on interfaces [%s]", strings.Join(capturing, ","))
+	logging.Logger().Infof("attempting capture on interfaces [%s]", strings.Join(capturing, ","))
 
 	sigExitChan := make(chan os.Signal, 1)
 	signal.Notify(sigExitChan, unix.SIGTERM, os.Interrupt)
@@ -175,14 +177,12 @@ func (c *Capture) Run() (err error) {
 		go func(l *link.Link) {
 			defer wg.Done()
 
-			for _, skipLink := range skipped {
-				if l.Name == skipLink {
-					return
-				}
+			if slices.Contains(skipped, l.Name) {
+				return
 			}
 
 			if isUp, err := l.IsUp(); err != nil || !isUp {
-				logger.Warnf("skipping listener on non-up interface `%s`", l.Name)
+				logging.Logger().Warnf("skipping listener on non-up interface `%s`", l.Name)
 				return
 			}
 
@@ -190,12 +190,12 @@ func (c *Capture) Run() (err error) {
 			if c.useRingBuffer {
 				listener, err = afring.NewSourceFromLink(l, afring.CaptureLength(filter.CaptureLengthMinimalIPv6Transport))
 				if err != nil {
-					logger.Errorf("error starting listener (with ring buffer) on `%s`: %s", l.Name, err)
+					logging.Logger().Errorf("error starting listener (with ring buffer) on `%s`: %s", l.Name, err)
 				}
 			} else {
 				listener, err = afpacket.NewSourceFromLink(l)
 				if err != nil {
-					logger.Errorf("error starting listener (no ring buffer) on `%s`: %s", l.Name, err)
+					logging.Logger().Errorf("error starting listener (no ring buffer) on `%s`: %s", l.Name, err)
 				}
 			}
 			listeners = append(listeners, listener)
@@ -203,19 +203,19 @@ func (c *Capture) Run() (err error) {
 			var nErr int
 			if c.useZeroCopy {
 				for {
-					if err := listener.NextPacketFn(func(payload []byte, totalLen uint32, pktType byte, ipLayerOffset byte) error {
+					if err := listener.NextPacketFn(func(payload []byte, _ uint32, pktType byte, ipLayerOffset byte) error {
 						if c.logPacketPayload {
-							logger.Infof("[%s] Got %v / %d", l.Name, payload[ipLayerOffset:ipLayerOffset+16], pktType)
+							logging.Logger().Infof("[%s] Got %v / %d", l.Name, payload[ipLayerOffset:ipLayerOffset+16], pktType)
 						}
 						return nil
 					}); err != nil {
 						if errors.Is(err, capture.ErrCaptureStopped) {
-							logger.Infof("gracefully stopped capture on `%s`", l.Name)
+							logging.Logger().Infof("gracefully stopped capture on `%s`", l.Name)
 							return
 						}
 						nErr++
 						if nErr >= c.maxIfaceErrors {
-							logger.Errorf("too many errors (%d) on `%s`, stopping capture", nErr, l.Name)
+							logging.Logger().Errorf("too many errors (%d) on `%s`, stopping capture", nErr, l.Name)
 						}
 					}
 				}
@@ -224,17 +224,17 @@ func (c *Capture) Run() (err error) {
 					pkt, err := listener.NextPacket(nil)
 					if err != nil {
 						if errors.Is(err, capture.ErrCaptureStopped) {
-							logger.Infof("gracefully stopped capture on `%s`", l.Name)
+							logging.Logger().Infof("gracefully stopped capture on `%s`", l.Name)
 							return
 						}
 						nErr++
 						if nErr >= c.maxIfaceErrors {
-							logger.Errorf("too many errors (%d) on `%s`, stopping capture", nErr, l.Name)
+							logging.Logger().Errorf("too many errors (%d) on `%s`, stopping capture", nErr, l.Name)
 						}
 					}
 
 					if c.logPacketPayload {
-						logger.Infof("[%s] Got IP layer %v / %d", l.Name, pkt.IPLayer()[:16], pkt.Type())
+						logging.Logger().Infof("[%s] Got IP layer %v / %d", l.Name, pkt.IPLayer()[:16], pkt.Type())
 					}
 				}
 			}
@@ -249,12 +249,12 @@ func (c *Capture) Run() (err error) {
 		for _, listener := range listeners {
 			stats, err := listener.Stats()
 			if err != nil {
-				logger.Errorf("failed to retrieve socket stats: %s", err)
+				logging.Logger().Errorf("failed to retrieve socket stats: %s", err)
 			}
-			logger.Infof("Packet stats for %s: %#v", listener.Link().Name, stats)
+			logging.Logger().Infof("Packet stats for %s: %#v", listener.Link().Name, stats)
 
 			if err := listener.Close(); err != nil {
-				logger.Errorf("failed to gracefully stop capture: %s", err)
+				logging.Logger().Errorf("failed to gracefully stop capture: %s", err)
 			}
 		}
 	}()
